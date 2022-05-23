b3eb1623ec9eb1f481b35863945b3010763e1970
billydean <billy.goehring@gmail.com>
2022-05-22 17:08:52 -0700

    setting up traits

---
+++ src/notes.js
@@ -0,0 +1,536 @@
+/**
+Age:
+    6-9     140
+    10-13   125
+    14-17   125
+    18-25   200
+    26-30   60
+    31-40   80
+    41-50   60
+    51-60   40
+    61-70   20
+    71-80   10
+    81+     1
+
+Pronouns:
+    he/him      105
+    she/her     100
+    they/them   10
+Eyes:
+    large
+    small
+    narrow
+    squinty
+    round
+    wide-set
+    close-set
+    deep-set
+    sunken
+    bulging
+    hooded
+    heavy-lidded
+    dull
+    bright
+    cloudy
+    beady
+    steely
+Eyelashes:
+    long
+    sweeping
+    sparse
+    no-comment
+Eye Color:
+    light blue  39      1
+    blue        900     22
+    blue-gray   230     5
+    gray-green  130     3
+    green       299     7
+    hazel       552     13
+    light brown 108     2
+    brown       4855    121
+    dark brown  478     11
+Eyebrows:
+    arched
+    straight
+    sparse
+    plucked
+    thick
+    faint
+    thin
+    unruly
+    bushy
+    uni-brow
+    no-comment
+Skin Color:
+    ruddy
+    olive
+    fair
+    light-skinned
+    pale
+    copper
+    bronze
+    amber
+    ebony
+Skin Quality-Face:
+    tatooed
+    sunburned
+    pitted
+    blemished
+    pockmarked
+    mottled
+    dull
+    poreless
+    with large pores
+    glowing
+    dewy
+    firm
+    dry
+    loose
+    smooth
+    droopy
+    sagging
+    leathery
+    wrinkled
+    lined
+    moisturized
+    no-comment
+Scars:
+Features:
+    dimples
+    freckles
+    visible adam's apple
+    no-comment
+Face Structure:
+    square
+    round
+    oval
+    narrow
+    heart-shaped
+Forehead:
+    high
+    low
+    prominent brow
+    no-comment
+Cheeks:
+    high cheekbones
+    angular cheekbones
+    hollow cheeks
+Jaw:
+    square
+    chiseled
+    rounded
+    jowly
+Chin:
+    pointed
+    jutting
+    weak
+    receding
+    double
+    cleft
+    dimpled
+Nose:
+    snub
+    dainty
+    button
+    turned-up
+    long
+    broad
+    thin
+    straight
+    pointed
+    crooked
+    roman
+    bulbous
+    flared
+    hawk
+    strong
+    no-comment
+Lips:
+    thin
+    narrow
+    full
+    lush
+    cupid's bow
+    dry
+    cracked
+    chapped
+    moist
+    glossy
+    no-comment
+Teeth:
+    straight
+    gapped
+    gleaming white
+    crooked
+    overbite
+    underbite
+    no-comment
+Facial Hair:
+    none
+    clean-shaven
+    beard
+    neck beard
+    goatee
+    moustache
+    sideburns
+    stubble
+    five o' clock shadow
+Hair Texture:
+    Straight
+    Wavy
+    Curly
+    Very Curly
+    Coiled
+Hair Length
+    Bald * 
+    Very Short
+    Short
+    Medium
+    Chin-length
+    Shoulder-length
+    Mid-back
+    Very long
+
+Hair Color:
+    black
+    dark brown
+    brown
+    light brown
+    auburn
+    ash brown
+    dirty blond
+    copper red
+    golden blonde
+    platinum blonde
+    gray
+    salt-and-pepper
+    white
+    blue
+    green
+    purple
+    pink
+Hair Color-modifiers:
+    tinged with gray
+    with dark roots
+    with streaks of _____
+    with ______ highlights
+
+Hair Amount:
+    full head
+    thin
+    thinning
+    receding hairline
+    widow's peak
+Hair Style:
+    crewcut     (very short)
+    buzz cut    (very short)
+    bowl cut    (short)
+    caesar cut  (short)
+    bob         (short)
+    pixie       (very short)
+    tonsure     (***)
+    bouffant
+    bun
+    pigtails
+    comb over (thinning)
+    crown braid
+    ponytail
+    ducktail
+    feathered
+    slicked-back
+    mop-top
+    blowout
+    beehive
+    locks
+    top-knot
+    box braids
+    afro
+    french braid
+    mullet
+    pompadour
+    waves
+    ringlets
+    side part
+    middle part
+
+    rattail
+    frosted tips
+    bangs
+    finger waves
+
+    asymmetric
+    
+Hair Appearance:
+    Dry
+    Oily
+    glossy
+    Silky
+    windblown
+    neatly combed
+    unkempt
+    tangled
+    no-comment
+
+Height:
+    Petite
+    Short
+    Tall
+    Very Tall
+    no-comment
+Build:
+    Compact
+    Big
+    Large
+    Burly
+    Beefy
+    Bulky
+    Brawny
+    Barrel-chested
+    Heavy-set
+    Chubby
+    Portly
+    Thick
+    Stout
+    Full-figured
+    Ample
+    Voluptuous
+    Curvy
+    Plump
+    Long-legged
+    Lanky
+    Lithe
+    Lean
+    Slim
+    Slender
+    Trim
+    Thin
+    Skinny
+    Gaunt
+    Bony
+    Solid
+    Sinewy
+    Strapping
+    Hulking
+    Fit
+    Athletic
+    Toned
+    Built
+    Muscular
+    Chiseled
+    Broad-shouldered
+    Slouching
+    Bowlegged
+Appearance:
+    Beautiful
+    Well-groomed
+    Handsome
+    Tired
+
+Professions:
+    Cook
+    Blacksmith
+    Carpenter
+    Armourer
+    Grocer
+    Merchant
+    Belt Maker
+    Tax Collector
+    Tanner
+    Locksmith
+    Roofer
+    Wheelwright
+    Cobbler
+    Watchman
+    Farmer
+    Mason
+    Winemaker
+    Weaver
+    Stonemason
+    Baker
+    Butcher
+    Barber
+    Basketmaker
+    Beerbrewer
+    Bowyer
+    Chandler
+    Casketmaker
+    Milliner
+    Clerk
+    Chaplain
+    Coiner
+    Drover
+    Dyer
+    Fisher
+    Fishmonger
+    Fletcher
+    Founder
+    Furrier
+    Fruiterer
+    Haberdasher
+    Haymonger
+    Hooder
+    Horsemonger
+    Hosier
+    Huckster
+    Imager
+    Innholder
+    Jeweler
+    Joiner
+    Laborer
+    Lacemaker
+    Launderer
+    Leatherworker
+    Lanternmaker
+    Marshal
+    Marbler
+    Malemaker
+    Mason
+    Mercer
+    Miller
+    Milkman
+    Moneyer
+    Monk
+    Musician
+    Netter
+    Notary
+    Parchment-maker
+    Pastrymaker
+    Paternoster
+    Physician
+    Piebaker
+    Platemaker
+    Porter
+    Potter
+    Poulterer
+    Priest
+    Purser
+    Quilter
+    Roper
+    Salter
+    Sawyer
+    Scrivener
+    Seal-maker
+    Sailor
+    Shapster
+    Shipwright
+    Smith
+    Tableter
+    Tailor
+    Tanner
+    Tapster
+    Taverner
+    Tiler
+    Vintner
+    Waterbearer
+    Weaver
+
+Personality (16PF):
+{ignore 3-8; below low | high}
+    A (Warmth):
+        distant, cool, reserved, detached, formal, aloof;
+        warm, outgoing, easygoing
+    C (Stability):
+        reactive, unstable, easily upset, impressionable;
+        stable, adaptive, mature, even-keeled
+    E (Dominance):
+        deferential, cooperative, submissive, obedient, docile, accommodating;
+        dominant, forceful, assertive, aggressive, competitive, stubborn, bossy;
+    F (Liveliness):
+        serious, restrained, taciturn, introspective, silent;
+        lively, animated, spontaneous, enthusiastic, happy-go-lucky, cheerful, expressive, impulsive
+    G (Rule-Conscious):
+        expedient, non-conforming, self-indulgent, irreverent;
+        conscientious, dutiful, conforming, moralistic, rule-bound;
+    H (Social Boldness):
+        shy, timid, hesitant, intimidated, wary;
+        bold, venturesome, thick-skinned, uninhibited, confident;
+    I (Sensitivity):
+        utilitarian, objective, tough-minded, self-reliant, no-nonsense, rough;
+        sensitive, aesthetic, sentimental, intuitive, refined
+    L (Vigilance):
+        trusting, accepting, easy, laid-back;
+        vigilant, suspicious, skeptical, distrustful, oppositional
+    M (Abstractedness):
+        grounded, practical, conventional;
+        abstract, imaginative, absentminded, impractical
+    N (Privateness):
+        forthright, genuine, guileless, naive, unpretentious;
+        private, discreet, shrewd, astute, diplomatic
+    O (Apprehension):
+        self-assured, unworried, complacent, secure;
+        apprehensive, self-doubting, worried, insecure
+    Q1 (Openness to Change):
+        traditional, conservative, stubborn;
+        experimental, critical, flexible, freethinking        
+    Q3 (Perfectionism):
+        impulsive, careless, lax, chaotic, disorganized;
+        perfectionist, compulsive, disciplined, controlled, precise
+    Q4 (Tension):
+        relaxed, tranquil, patient;
+        tense, driven, impatient, frustrated, over-wrought
+
+Virtues:
+    Courageous
+    Temperate
+    Honest
+    Witty
+    Friendly
+    Modest
+    Righteous
+    Generous
+    Self-aware
+    Motivated
+    Patient
+    Kind
+    Yielding
+    Polite
+    Forgiving
+    Compassionate
+    Responsible
+    Considerate
+
+Vices:
+    Rash
+    Rude
+    Impatient
+    Oblivious
+    Merciless
+    Irresponsible
+    Inconsiderate
+    Wasteful
+    Miserly
+    Vulgar
+    Petty
+    Vain
+    Self-deprecating
+    Sycophant
+    Unambitious
+    Short-fused
+    Callous
+    Boastful
+    Boring
+    Foolish
+    Abrasive
+    Obsequious
+    Shy
+    Shameless
+    Envious
+    Spiteful
+Education Opportunity:
+    Yes
+    No
+Education:
+    Formal
+    Apprenticeship
+    Informal
+    None
+
+Health
+Immediate Goal(s)
+Long-term Goal(s)
+IQ
+Education
+Equipment?
+Belongings?
+Clothing
+ */
