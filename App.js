8e21e220629c0800f431c8a9c31be99ae795501b
billydean <billy.goehring@gmail.com>
2022-06-02 11:09:26 -0700

    brought in old code to clean up

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Setting from './components/Setting';
+import Description from './components/Description';
+import Comportment from './components/Comportment';
+
+function App() {
+    return (
+      <div>
+          <h1>Who Is It?</h1>
+        <Setting />
+        <Description />
+        <Comportment />
+      </div>
+    );
+  }
+  
+  export default App;
+  
---
+++ README.md
@@ -0,0 +1,8 @@
+# Chance Encounters
+who are you meeting
+## Project Description
+Chance Encounters is...
+what it does
+why it is the way it is
+challenges
+invitation/cta
