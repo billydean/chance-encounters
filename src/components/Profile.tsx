134b1fc761a80117d46d7c48ea9a007a7ef3e2a6
billydean <billy.goehring@gmail.com>
2022-06-06 14:11:54 -0700

    set up svg on profile comp

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,9 +1,37 @@
 import * as React from 'react';
+import * as path from '../images/paths';
 
 function Profile () {
     return (
-      <div>
-
+      <div className='profile-container'>
+        <svg 
+          className='profile-svg' 
+          viewBox="0 0 567 779.625" 
+          fillRule="nonzero"
+          clipRule="evenodd"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+        > 
+          <g>
+          <path 
+            d={path.dummy} 
+            fill="#fff8de"
+            fillRule='evenodd'
+            opacity='1'
+            stroke='none'
+          />
+          <path 
+            d={path.dummy2} 
+            fill="none"
+            fillRule='evenodd'
+            opacity='1'
+            stroke='#000000'
+            strokeLinejoin='round'
+            strokeWidth='13.9159'
+          />
+          </g>
+        </svg>
       </div>
     );
   }
